331c79b539f9f7eb7cc717541bdfb4c58e90b811
jamirou <devjamiro@gmail.com>
2023-11-13 14:34:18 -0300

    dialog configuration

--- app/src/main/java/com/jamirodev/todocompose/MainActivity.kt
+++ app/src/main/java/com/jamirodev/todocompose/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.jamirodev.todocompose.newtask.ui.TaskScreen
 import com.jamirodev.todocompose.newtask.ui.TaskViewModel
 import com.jamirodev.todocompose.ui.theme.ToDoComposeTheme
@@ -28,8 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    TaskScreen(taskViewModel)
-                    TaskScreen()
+                    TaskScreen(taskViewModel)
                 }
             }
         }
--- app/src/main/java/com/jamirodev/todocompose/newtask/ui/TaskViewModel.kt
+++ app/src/main/java/com/jamirodev/todocompose/newtask/ui/TaskViewModel.kt
@@ -1,7 +1,27 @@
 package com.jamirodev.todocompose.newtask.ui
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class TaskViewModel @Inject constructor():ViewModel() {
+
+    private val _showDialog = MutableLiveData<Boolean>()
+    val showDialog:LiveData<Boolean> = _showDialog
+    fun onDialogClosed() {
+        _showDialog.value = false
+    }
+
+    fun onTaskCreated(task: String) {
+        _showDialog.value = false
+        Log.i("Jamiro", task)
+    }
+
+    fun onShowDialogClick() {
+        _showDialog.value = true
+    }
+
+
 }
