818d0d30d6b9a17867a92fcc47104acde5298b76
jamirou <devjamiro@gmail.com>
2023-11-13 12:40:13 -0300

    TaskScreen ui, dialog.

--- app/src/main/java/com/jamirodev/todocompose/newtask/ui/TaskScreen.kt
+++ app/src/main/java/com/jamirodev/todocompose/newtask/ui/TaskScreen.kt
@@ -1,7 +1,95 @@
 package com.jamirodev.todocompose.newtask.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+@Preview
+@Composable
+fun TaskScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        AddTaskDialog(show = true, onDismiss = {}, onTaskAdded = {})
+        FabDialog(Modifier.align(Alignment.BottomEnd))
+    }
+}
 
 @Composable
-fun TaskScreen(taskViewModel: TaskViewModel) {
+fun FabDialog(modifier: Modifier) {
+    FloatingActionButton(
+        onClick = { /*TODO*/ },
+        modifier = modifier
+            .padding(16.dp)
+    ) {
+        Icon(Icons.Filled.Add, contentDescription = "Add")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddTaskDialog(show: Boolean, onDismiss: () -> Unit, onTaskAdded:(String) -> Unit) {
+    var myTask by remember { mutableStateOf("") }
+    if (show) {
+        Dialog(onDismissRequest = { onDismiss() }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(18.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Add your task",
+                    fontSize = 18.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontWeight = FontWeight.Bold
+                )
+                UseSpace(space = 16)
+                TextField(
+                    value = myTask,
+                    onValueChange = { myTask = it },
+                    singleLine = true,
+                    maxLines = 1,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                UseSpace(space = 16)
+                Button(onClick = {
+                    onTaskAdded(myTask)
+                }, modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Sed Task")
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun UseSpace(space: Int) {
+    Spacer(modifier = Modifier.size(space.dp))
 }
