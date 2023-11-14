7ef81fe4adae13dfdba16380b75a276d87881a2e
jamirou <devjamiro@gmail.com>
2023-11-14 20:27:25 -0300

    flow fix1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,8 @@
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id ("com.google.dagger.hilt.android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -44,23 +42,22 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.3" /*1.4.3*/
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
 }
+
 
 dependencies {
 
-    //dagger
-    implementation ("com.google.dagger:hilt-android:2.48.1")
-    kapt ("com.google.dagger:hilt-compiler:2.48.1")
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.core:core-ktx:1.9.0") /*androidx.core:core-ktx:1.9.0*/
+    implementation("androidx.activity:activity-compose:1.6.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -74,12 +71,16 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    //dagger
+    implementation("com.google.dagger:hilt-android:2.41")
+    ksp("com.google.dagger:hilt-compiler:2.41")
+
     //LiveData
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2") /*2.5.1*/
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1") /*2.5.1*/
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
     //Room
     implementation("androidx.room:room-runtime:2.6.0")
-    kapt("androidx.room:room-compiler:2.6.0")
+    ksp("androidx.room:room-compiler:2.5.0")
     implementation("androidx.room:room-ktx:2.6.0")
 }
--- app/src/main/java/com/jamirodev/todocompose/ToDoApp.kt
+++ app/src/main/java/com/jamirodev/todocompose/ToDoApp.kt
@@ -3,5 +3,6 @@ package com.jamirodev.todocompose
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//@HiltAndroidApp
 @HiltAndroidApp
 class ToDoApp:Application()
